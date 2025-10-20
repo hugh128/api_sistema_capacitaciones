3a8995157279d745c918eecf97744141ccd2f968
Hugh <hugh128ord@gmail.com>
2025-10-19 21:20:50 -0600

    Agregado port del front en varibles de entorno

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT || 3001;
+  const FRONT_PORT = process.env.FRONT_PORT || '3000';
 
   app.enableCors({
-    origin: `http://localhost:${port}`,
+    origin: `http://localhost:${FRONT_PORT}`,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
@@ -17,6 +17,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new JwtAuthGuard(reflector));
+  const port = process.env.PORT || 3001;
   await app.listen(port);
 
   console.log(`Servidor corriendo en el puerto ${port}`);
